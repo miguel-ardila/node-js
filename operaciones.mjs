195593e329c921dcaf7eff2880afbd6a7e97487c
Miguel Ardila <miguel_sxe@hotmail.com>
2023-09-05 22:11:55 -0500

    MOdulesES operaciones.mjs

---
+++ operaciones.mjs
@@ -0,0 +1,19 @@
+function suma (num1, num2) {
+    const result = num1 + num2;
+    return result;
+}
+function rest (num1, num2) {
+    const result = num1 - num2;
+    return result;
+}
+function mult (num1, num2) {
+    const result = num1 * num2;
+    return result;
+}
+function divi (num1, num2) {
+    const result = num1 / num2;
+    return result;
+}
+export {suma, rest, mult, divi} //module ES
+
+//module.exports = {suma, rest, mult, divi}
